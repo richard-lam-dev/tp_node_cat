4f822e14001e0fdea560a9807b4a7c99bfbd075f
richard-lam-dev <richard.lam@eemi.com>
2024-03-27 18:01:32 +0100

    backend - récupération des données de l'api et implémentation de route

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-import fetch from "node-fetch";
 const express = require("express");
 const app = express();
 const port = 3000;
 
-// Fetch a key api
+// On récupère les données de l'API Rick and Morty pour les personnages
+fetch("https://rickandmortyapi.com/api/character")
+// On convertit les données en JSON
+  .then((response) => response.json())
+  // On récupère les données
+  .then((data) => {
+    console.log('affiche data', data)
 
-const response = await fetch("https://rickandmortyapi.com/api");
+    // Route pour récupérer tous les personnages
+    app.get("/character", (req, res) => {
+      // On envoie les données au format JSON
+      res.json(data);
+    });
+  })
+  // En cas d'erreur on affiche un message d'erreur dans la console du serveur
+  .catch((error) => {
+    console.error("Erreur lors de la récupération des données :", error);
+  });
 
-const data = await response.json();
-
-app.get("/", (req, res) => {
-  res.json({ data });
-});
+  // Route pour récupérer un personnage par son id
+app.get("/character/:id", (req, res) => {
+  // On récupère l'id du personnage dans les paramètres de la requête et on le stocke dans une variable
+  const id = req.params.id;
+ // On récupère les données de l'API pour le personnage correspondant à l'id
+  fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    // On convertit les données en JSON
+    .then((response) => response.json())
+    // On récupère les données
+    .then((data) => {
+      // On envoie les données au format JSON
+      res.json(data);
+    })
+    // En cas d'erreur on affiche un message d'erreur dans la console du serveur
+    .catch((error) => {
+      console.error("Erreur lors de la récupération des données :", error);
+    });
+}
+);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
