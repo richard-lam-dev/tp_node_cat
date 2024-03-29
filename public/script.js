b9b6ecc823b56beba4eb6be1f3e05390c43402f6
richard-lam-dev <richard.lam@eemi.com>
2024-03-29 11:16:10 +0100

    enlever les commentaires

--- public/script.js
+++ public/script.js
@@ -3,34 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   async function fetchCharacters() {
     const url = '/character';
     try {
-        const response = await fetch(url); // Effectue une requête GET vers l'API
-        const data = await response.json(); // Convertit la réponse en JSON
-        displayCharacters(data.results); // Appelle la fonction pour afficher les personnages
+        const response = await fetch(url); 
+        const data = await response.json(); 
+        displayCharacters(data.results); 
     } catch (error) {
-        console.error('Erreur lors de la récupération des personnages:', error); // Gère les erreurs
+        console.error('Erreur lors de la récupération des personnages:', error); 
     }
   }
 
-  // Fonction pour afficher les personnages sur la page
   function displayCharacters(characters) {
-    const container = document.getElementById('characterContainer'); // Sélectionne le conteneur des personnages
-    container.innerHTML = ''; // Nettoie le contenu existant
+    const container = document.getElementById('characterContainer');
+    container.innerHTML = ''; 
 
-    // Parcourt les personnages et les affiche par groupe de trois
     for (let i = 0; i < characters.length; i += 3) {
-        // Crée une nouvelle ligne
+  
         const row = document.createElement('div');
         row.classList.add('character-row');
 
-        // Ajoute jusqu'à trois personnages à la ligne
         for (let j = i; j < i + 3 && j < characters.length; j++) {
             const character = characters[j];
 
-            // Crée une div pour chaque personnage
             const characterWrapper = document.createElement('div');
             characterWrapper.classList.add('character-wrapper');
 
-            // Crée une div pour le personnage et remplie avec les détails
             const characterDiv = document.createElement('div');
             characterDiv.classList.add('character');
             characterDiv.innerHTML = `
@@ -40,54 +35,35 @@ document.addEventListener('DOMContentLoaded', () => {
                 <p>Espèce: ${character.species}</p>
             `;
 
-            // Ajoute la div du personnage à la div parente
             characterWrapper.appendChild(characterDiv);
 
-            // Ajoute la div parente à la ligne
             row.appendChild(characterWrapper);
         }
 
-        // Ajoute la ligne au conteneur principal
         container.appendChild(row);
     }
-
-    // Affiche chaque personnage individuellement
-    characters.forEach(character => {
-        const element = document.createElement('div');
-        element.innerHTML = `
-            <h2>${character.name}</h2>
-            <img src='${character.image}' alt="${character.name}" style="width: 180px; height: auto;">
-            <p>Statut: ${character.status}</p>
-            <p>Espèce: ${character.species}</p>
-         `;
-        container.appendChild(element);
-    });
   }
 
-  // Appelle fetchCharacters au chargement de la page
   fetchCharacters();
     
   // Ajoute un gestionnaire d'événement pour le formulaire d'e-mail
   document.getElementById("emailForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Empêche le formulaire de se soumettre normalement
+    event.preventDefault(); 
     
-    const formData = new FormData(this); // Récupère les données du formulaire
-    console.log(formData.get('subject')); // Affiche le sujet du message dans la console
-    console.log(formData.get('message')); // Affiche le corps du message dans la console
+    const formData = new FormData(this);
     
     // Envoie les données du formulaire au serveur
     fetch("/send-mail", {
-      method: "POST", // Utilise la méthode POST
-      body: new URLSearchParams(formData) // Convertit les données du formulaire en format x-www-form-urlencoded
+      method: "POST", 
+      body: new URLSearchParams(formData) 
     })
     .then(response => response.text())
     .then(data => {
-      console.log(data); // Affiche la réponse du serveur dans la console
-      alert(data); // Affiche un message à l'utilisateur
+      alert(data); 
     })
     .catch(error => {
-      console.error("Erreur lors de l'envoi du formulaire :", error); // Gère les erreurs
-      alert("Erreur lors de l'envoi du formulaire. Veuillez réessayer."); // Affiche un message d'erreur à l'utilisateur
+      console.error("Erreur lors de l'envoi du formulaire :", error); 
+      alert("Erreur lors de l'envoi du formulaire. Veuillez réessayer.");
     });
   });    
 });
--- server.js
+++ server.js
@@ -1,73 +1,68 @@
-const express = require("express"); // Importe le module Express
-const app = express(); // Crée une instance de l'application Express
-const path = require("path"); // Importe le module path pour la gestion des chemins de fichiers
-const port = 3000; // Définit le port sur lequel le serveur écoutera
-const { Resend } = require('resend'); // Importe la classe Resend de la bibliothèque 'resend'
-require('dotenv').config(); // Charge les variables d'environnement à partir du fichier .env
-const resend = new Resend(process.env.RESEND_API_KEY); // Crée une nouvelle instance de Resend avec la clé API provenant des variables d'environnement
-
-app.use(express.static('public')); // Configure Express pour servir les fichiers statiques du dossier "public"
-app.use(express.json()); // Middleware pour traiter les données JSON
+const express = require("express"); 
+const app = express(); 
+const path = require("path"); 
+const port = 3000; 
+const { Resend } = require('resend'); 
+require('dotenv').config(); 
+const resend = new Resend(process.env.RESEND_API_KEY);
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true })); // Middleware pour traiter les données URL-encoded
 
 // Gestion de route pour la page d'accueil "/"
 app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html")); // Renvoie le fichier index.html situé dans le dossier racine du projet
+  res.sendFile(path.join(__dirname, "index.html")); 
 });
 
 // Route pour afficher les personnages
 app.get("/character", async (req, res) => {
   try {
-    // Effectue une requête vers l'API pour récupérer la liste des personnages
     const response = await fetch("https://rickandmortyapi.com/api/character");
-    const data = await response.json(); // Convertit la réponse en JSON
-    res.json(data); // Renvoie les données au format JSON
+    const data = await response.json(); 
+    res.json(data); 
   } catch (error) {
     console.error("Erreur lors de la récupération des données :", error);
-    res.status(500).json({ error: "Erreur lors de la récupération des données" }); // Renvoie une erreur 500 en cas d'échec
+    res.status(500).json({ error: "Erreur lors de la récupération des données" }); 
   }
 });
 
 // Gestion de route pour "/character/:id"
 app.get("/character/:id", async (req, res) => {
-  const id = req.params.id; // Récupère l'ID du personnage à partir de la requête
+  const id = req.params.id; 
   try {
-    // Effectue une requête vers l'API pour récupérer les détails d'un personnage spécifique en fonction de son ID
     const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
-    const data = await response.json(); // Convertit la réponse en JSON
-    res.json(data); // Renvoie les données du personnage au format JSON
+    const data = await response.json(); 
+    res.json(data); 
   } catch (error) {
     console.error("Erreur lors de la récupération des données du personnage :", error);
-    res.status(500).json({ error: "Erreur lors de la récupération des données du personnage" }); // Renvoie une erreur 500 en cas d'échec
+    res.status(500).json({ error: "Erreur lors de la récupération des données du personnage" }); 
   }
 });
 
 // Gestion de route pour "/send-mail"
 app.post("/send-mail", async (req, res) => { 
-  const { subject, message } = req.body; // Récupère le sujet et le message du corps de la requête
+  const { email, subject, message } = req.body; 
   
   try {
-    // Envoie un e-mail en utilisant la bibliothèque Resend avec les données du formulaire
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: ['richard.lam@eemi.com'],
-      subject: subject, // Utilise le sujet récupéré du formulaire
-      html: message, // Utilise le corps du message récupéré du formulaire
+      email: email,
+      subject: subject, 
+      html: message,
     });
 
     if (error) {
       console.error({ error });
-      return res.status(500).send('Erreur lors de l\'envoi de l\'email'); // Renvoie une erreur 500 en cas d'échec
+      return res.status(500).send('Erreur lors de l\'envoi de l\'email'); 
     }
 
-    res.send('Email envoyé !'); // Renvoie une réponse indiquant que l'e-mail a été envoyé avec succès
+    res.send('Email envoyé !'); 
   } catch (error) {
     console.error("Erreur lors de l'envoi de l'email :", error);
-    res.status(500).send('Erreur lors de l\'envoi de l\'email'); // Renvoie une erreur 500 en cas d'échec
+    res.status(500).send('Erreur lors de l\'envoi de l\'email'); 
   }
 });
 
-// Démarre le serveur Express et écoute les connexions entrantes sur le port spécifié
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`); // Affiche un message indiquant que le serveur a démarré avec succès
+  console.log(`App listening on port ${port}`); 
 });
